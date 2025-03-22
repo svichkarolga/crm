a2205477b2110953f0282f175bb3224f44d3ec2b
svichkar olga <syns41@gmail.com>
2025-03-23 01:33:43 +0200

    add button

--- src/app/Components/Button.tsx
+++ src/app/Components/Button.tsx
@@ -8,5 +8,14 @@ export interface ButtonProps
 }
 
 export default function Button({ disabled, ...rest }: ButtonProps) {
-  return <Button {...rest} className={clsx('py-2.5, px-5')} />;
+  return (
+    <Button
+      {...rest}
+      className={clsx(
+        'py-2.5 px-5 bg-gray-900 text-zinc-50 text-base text-center font-medium rounded',
+        !disabled && 'hover:bg-gray-800 active:bg-gray-950',
+        disabled && 'text-zinc-100',
+      )}
+    />
+  );
 }
