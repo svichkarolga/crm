e83406ff5fccafc7e7345ed53e86a7c1a9664d4d
svichkar olga <syns41@gmail.com>
2025-03-24 01:29:31 +0200

    add main btm, modal with inputs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,25 @@
 import StatusLabel, { Status } from './Components/StatusLabel';
+<<<<<<< Updated upstream
 import AddCompanyButton from '@/app/Components/AddCompanyButton';
+=======
+>>>>>>> Stashed changes
 
 export default function Home() {
   return (
     <main>
       <h1 className="text-xl">Home page </h1>
+<<<<<<< Updated upstream
       <AddCompanyButton />
       {/*<StatusLabel status={Status.Active}>Active</StatusLabel>*/}
       {/*<StatusLabel status={Status.NotActive}>NotActive</StatusLabel>*/}
       {/*<StatusLabel status={Status.Pending}>Pending</StatusLabel>*/}
       {/*<StatusLabel status={Status.Suspended}>Suspended</StatusLabel>*/}
+=======
+      <StatusLabel status={Status.Active}>Active</StatusLabel>
+      <StatusLabel status={Status.NotActive}>NotActive</StatusLabel>
+      <StatusLabel status={Status.Pending}>Pending</StatusLabel>
+      <StatusLabel status={Status.Suspended}>Suspended</StatusLabel>
+>>>>>>> Stashed changes
     </main>
   );
 }
