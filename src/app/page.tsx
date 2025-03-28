793af46c458614d3efdf90543fda720f4d7310a0
svichkar olga <syns41@gmail.com>
2025-03-29 01:05:02 +0200

    wrote some info to readme file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,18 @@ import AddCompanyButton from '@/app/Components/AddCompanyButton';
 
 export default function Home() {
   return (
-    <main>
-      <h1 className="text-xl">Home page </h1>
+    <main className="min-h-screen flex items-center justify-center bg-gradient-to-r from-blue-200 to-pink-200">
+      <div className="text-center p-6 bg-white rounded-lg shadow-lg max-w-lg w-full">
+        <h1 className="text-4xl font-semibold text-blue-600 mb-6">
+          Welcome to the Home Page
+        </h1>
 
-      <AddCompanyButton />
+        <p className="text-lg text-gray-600 mb-8">
+          Here you can manage your companies and perform other actions.
+        </p>
+
+        <AddCompanyButton />
+      </div>
     </main>
   );
 }
